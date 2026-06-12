c0decfb571d487d3f0d5a5d638608fc308e05dfb
Maciej Mucha <mucha.maciej2000@gmail.com>
2026-06-12 23:19:14 +0200

    add base scene example program

---
+++ Manipulator.Examples/Program.cs
@@ -0,0 +1,13 @@
+using Manipulator.Examples.Programs;
+
+Console.WriteLine("=== Running example usage of library ===");
+Console.WriteLine();
+
+Console.WriteLine("=== Example 1: Basic scene ===");
+Console.WriteLine();
+
+BasicScene.Run();
+
+Console.WriteLine();
+Console.WriteLine("=== Example 1: Done ===");
+Console.WriteLine();
